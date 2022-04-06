d48361725391929ea74634ed0aafebc5d96864fd
supertree-emjay <emjay@supertree.co>
2022-04-06 20:29:17 +0900

    update package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: "imgix",
+    path: "http://localhost:3000/",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
